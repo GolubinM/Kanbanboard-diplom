9d0af99885d8e724f125dd32ec3c064e411f405f
Mihail Golubin <golm2020@gmail.com>
2023-01-03 22:45:44 +0300

    Добавлен класс task--backlog для новой задачи

--- js/modules/getTaskElement.js
+++ js/modules/getTaskElement.js
@@ -1,19 +1,13 @@
 export default function getTaskElement(content) {
   let insertPosition = document.querySelector(".taskboard__list");
-  
-  // let divNewTask = `<form class="task__form" aria-label="Форма задачи">
-  let divNewTask = `<div class="taskboard__item task">
+
+  let divNewTask = `<div class="taskboard__item task task--backlog">
   <div class="task__body">
     <p class="task__view">${content}</p>
     <input class="task__input" type="text" value="${content}">
   </div>
   <button class="task__edit" type="button" aria-label="Изменить"></button>
 </div>`;
-// </form>`;
   insertPosition.insertAdjacentHTML("afterbegin", divNewTask);
   return insertPosition.firstChild;
 }
-
-
-//   document.querySelector(".task--active").classList.remove('task--active'); //сделать задачу не активной
-//   document.querySelector("...").classList.add('taskboard__item task task--active'); //сделать задачу активной
