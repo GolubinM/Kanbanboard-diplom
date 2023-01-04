bcfcea8072e81912ecc996a275bdc66b3635c526
Mihail Golubin <golm2020@gmail.com>
2023-01-04 18:57:29 +0300

    Очистка корзины добавлена

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 import Task from "./modules/task.js";
+const btnClearBasket = document.querySelector(".button--clear");
+//------------------------------------------------------------------------
+// Создание задачи
 
 let inputForm = document.querySelector(".add-task__form");
 //Обработка создания нового элемента в поле Input
@@ -14,15 +17,24 @@ inputForm.addEventListener("submit", (event) => {
 //Обработка перехода на поле Input из редактируемых элементов task
 inputForm.addEventListener("click", () => {
   document.querySelectorAll(".task--active").forEach(function (item) {
-    item.querySelector(".task__view").textContent =
-      item.querySelector(".task__input").value;
+    item.querySelector(".task__view").textContent = item.querySelector(".task__input").value;
     item.classList.remove("task--active");
   });
 });
 
 //------------------------------------------------------------------------
+//Удаление задачи из корзины
 
-const tasksListElements = document.querySelectorAll(`.taskboard__list`);
+btnClearBasket.addEventListener("click", () => {
+  const deletedTasks = document.querySelectorAll(".task--basket"); // отбор удаляемых задач
+  deletedTasks.forEach((deletedTask) => {
+    deletedTask.parentNode.removeChild(deletedTask); // удаление html кода задач
+  });
+  hasTask(); // Отображение пустого элемента "Корзина пуста"
+});
+
+//------------------------------------------------------------------------
+const tasksListElements = document.querySelectorAll(".taskboard__list");
 const taskElements = document.querySelectorAll(".taskboard__item");
 taskElements.forEach((evt) => {
   if (!evt.classList.contains("task--empty")) {
@@ -41,6 +53,7 @@ document.addEventListener(`dragend`, (evt) => {
   const activeElement = document.querySelector(`.task--dragged`);
   evt.target.classList.remove(`task--dragged`);
   classByColumn(activeElement);
+  isBasketEmpty();
 });
 
 tasksListElements.forEach((tasksListElement) =>
@@ -48,7 +61,6 @@ tasksListElements.forEach((tasksListElement) =>
     if (evt.target.classList.contains("task")) {
       // Разрешаем сбрасывать элементы в эту область
       evt.preventDefault();
-
       // Находим перемещаемый элемент
       const activeElement = document.querySelector(`.task--dragged`);
       // Находим элемент, над которым в данный момент находится курсор
@@ -57,13 +69,11 @@ tasksListElements.forEach((tasksListElement) =>
       // 1. не на том элементе, который мы перемещаем,
       // 2. именно на элементе списка
       const isMoveable =
-        activeElement !== currentElement &&
-        currentElement.classList.contains(`task`);
+        activeElement !== currentElement && currentElement.classList.contains(`task`);
       // Если нет, прерываем выполнение функции
       if (!isMoveable) {
         return;
       }
-
       // Находим элемент, перед которым будем вставлять
       const nextElement =
         currentElement === activeElement.nextElementSibling
@@ -97,15 +107,9 @@ function classByColumn(activeElement) {
       break;
   }
   // замена старого класса новым
-  return activeElement.classList.replace(
-    activeElement.classList[2],
-    statusClassTask
-  );
+  return activeElement.classList.replace(activeElement.classList[2], statusClassTask);
 }
-
 //------------------------------------------------------------------------
-
-
 // function hasTask() - скрытие пустого элемента если в taskboard__group присутствуют более 1 элемента '.taskboard__item'.
 function hasTask() {
   const taskboardListCollection = document.querySelectorAll(".taskboard__list"); // создаем коллекцию колонок задач
@@ -113,6 +117,19 @@ function hasTask() {
     let countTask = taskboard.querySelectorAll(".task").length; //определяем количество элементов в колонке
     // скрытие или отображение пустого элемента при опустошении колонок
     const classesEmptyTasks = taskboard.querySelector(".task--empty").classList;
-    (countTask > 1) ? classesEmptyTasks.add("hidden-block") : classesEmptyTasks.remove("hidden-block");
+    countTask > 1
+      ? classesEmptyTasks.add("hidden-block")
+      : classesEmptyTasks.remove("hidden-block");
   });
 }
+
+//------------------------------------------------------------------------
+// Проверка корзину на пустоту
+function isBasketEmpty() {
+  const isEmpty = !Boolean(document.querySelector(".taskboard__list--trash").children.length - 1);
+  // const emptyTaskList = document.querySelector(".task--empty-trash").classList;
+  // isEmpty ? emptyTaskList.add("hidden") : emptyTaskList.remove("hidden");
+  btnClearBasket.disabled = isEmpty;
+  return isEmpty;
+}
+isBasketEmpty();
