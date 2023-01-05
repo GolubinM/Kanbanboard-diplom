79378abc7b9e50d40c43895dcec401046321aab3
Mihail Golubin <golm2020@gmail.com>
2023-01-05 18:04:28 +0300

    перенос eventListener и функций из экземпляров класса в script
    Уменьшение количества общих обработчиков событий.
    Вместо того, чтобы создавать обработчик для каждого экземпляра класса,
    создаем общий для всех экземпляров с выбором таргетов по атрибуту.
    Функции для изменения html переносим  из экземпляров класса в общий script.

--- js/modules/task.js
+++ js/modules/task.js
@@ -3,47 +3,10 @@ import getTaskElement from "./getTaskElement.js";
 export default class Task {
   constructor(taskContent) {
     this.content = taskContent;
-    this.task = undefined;
-    this.inputTask = undefined;
-  }
-
-  _add(task) {
-    return getTaskElement(task);
-  }
-
-  _changeActive(task) {
-    // отключает режим редактирования для всех задач, переключает режим для текущей задачи
-    // обновляет содержание задачи из поля input для редактируемых задач
-    const isActive = task.classList.contains("task--active");
-    document.querySelectorAll(".task--active").forEach(function (item) {
-      item.querySelector(".task__view").textContent = item.querySelector(".task__input").value;
-      item.classList.remove("task--active");
-    });
-    task.classList.toggle("task--active", !isActive);
-  }
-
-  _editTask(editBtn) {
-    this.task = editBtn.parentNode;
-    this._changeActive(this.task); //отключаем редактирование для всех остальных элементов
-    //переход к концу строки редактирования задачи
-    const inputTask = this.task.querySelector(".task__input");
-    inputTask.focus();
-    inputTask.selectionStart = inputTask.value.length;
   }
 
   init() {
-    this.task = this._add(this.content); // добавляет элемент с задачей в html
-    this.task.draggable = true; // делаем задачу перемещаемой
-    const editBtn = this.task.querySelector(".task__edit"); // определение кнопки редактирования задачи
-    editBtn.addEventListener("click", () => {
-      this._editTask(editBtn); // редактирование задачи
-    });
-    // завершение редактирования содержания задачи по Enter или Esc
-    this.inputTask = this.task.querySelector(".task__input");
-    this.inputTask.addEventListener("keyup", (event) => {
-      if (event.keyCode === 13 || event.keyCode === 27) {
-        this._changeActive(this.task);
-      }
-    });
+    // добавляет элемент с задачей в html, делаем элемент перемещаемым
+    getTaskElement(this.content).draggable = true; 
   }
 }
